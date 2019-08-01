f526b1f313d5b13149d2066f1a79e9f24678d6f8
Henrique Pereira <hlcpereira@outlook.com.br>
2019-08-01 16:18:53 -0300

    dipper: PixelExperience BringUp
    Signed-off-by: Henrique Pereira <hlcpereira@outlook.com.br>

--- device.mk
+++ device.mk
@@ -15,11 +15,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
-
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
+    $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
